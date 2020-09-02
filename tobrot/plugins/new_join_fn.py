6ea30b5e0d9fe9471c12f4e76cbe4ddff99b9d34
Usman Mughal <35332218+usmanmughalji@users.noreply.github.com>
2020-09-02 10:48:54 +0500

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,7 +38,7 @@ async def welcome_message_f(client, message):
     #message_id = 99
     # display the /help
     
-    await message.reply_text("""Welcome to Erick & Smokey Group""", disable_web_page_preview=True)
+    await message.reply_text("""Welcome to Team Drive Ultimate X""", disable_web_page_preview=True)
 
 
 async def help_message_f(client, message):
